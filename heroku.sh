fd397e3415f0ddde2716cfc5d4caf8c49fdf7e23
Jason McVetta <jason.mcvetta@gmail.com>
2012-07-11 01:28:02 -0700

    Add test for heroku toolbelt

--- heroku.sh
+++ heroku.sh
@@ -3,6 +3,11 @@
 # Script for first-time setup and deployment of this application to Heroku.
 #
 
+[ -e "`which heroku`" ] || (
+	echo "Could not find Heroku toolbelt."
+	echo "See https://toolbelt.heroku.com/ for installation instructions."
+); exit 1
+
 set -x
 
 heroku create --buildpack git://github.com/kr/heroku-buildpack-go.git
